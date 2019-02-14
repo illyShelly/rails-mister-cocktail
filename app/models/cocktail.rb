68ddf7e5c19cfb2df1d660a438f965b5e4894b04
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-14 18:58:56 +0100

    Check view index Cocktails

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
-  has_many :ingredients, through: doses
-  has_many :doses, dependent: destroy
+  # has_many :ingredients, through: doses
+  # has_many :doses, dependent: destroy
 
   validates :name, uniqueness: true, presence: true
   # later reviews
@@ -10,3 +10,5 @@ end
 # 2. has many doses -> destroyed when deleted cocktail
 # 3. deleting cocktails - should del associated doses
 # (but not the ingredients as they can be linked to other cocktails).
+
+# 4. uncomment relation when testing first Controller
