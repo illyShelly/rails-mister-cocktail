36426f976e6f4ff20000ff4a265120aabe0f2670
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-16 22:36:52 +0100

    Inputs default value, back link touch

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :cocktail
   validates :content, presence: true
-  validates :rating, inclusion: { in: 0..5 }
+  validates :rating, inclusion: { in: 1..5 }
 end
 
 
