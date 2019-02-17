48176501ef9a3fa7242fb5ad6d8686ce10d1c72e
Ilona Sellenberkova <ilona.sell@gmail.com>
2019-02-17 14:33:22 +0100

    get rid off redundant root and index action - 6 routes + root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "cocktails#index"
 
-  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
+  # when using root - homepage it's redundant to have in /cocktails
+  resources :cocktails, only: [:show, :new, :create, :destroy] do
     # resources :doses, only: [:create, :new]
     resources :doses, only: [:create]
     # will pass it on the show without new html form
@@ -14,9 +15,3 @@ end
 
 # GET "cocktails/42/doses/new"
 # POST "cocktails/42/doses"
-
-  # resources :cocktails, only: [:index, :show, :new, :create] do
-  #   resources :doses, only: [:create]
-  #   resources :reviews, only: [:create]
-  # end
-  # resources :doses, only: [:destroy]
